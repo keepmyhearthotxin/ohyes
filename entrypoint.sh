4993ec0b9d932bf3fb87adff39fbdb5bbf69b33a
keepmyhearthotxin <88603577+keepmyhearthotxin@users.noreply.github.com>
2022-02-25 11:57:00 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="eab2b090-dc4e-48cd-978b-ff55f1947301"
+  Vless_UUID="6b23ed38-03bd-488d-b5bd-f92e77a5631c"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="eab2b090-dc4e-48cd-978b-ff55f1947301"
+  Vmess_UUID="6b23ed38-03bd-488d-b5bd-f92e77a5631c"
 fi
 echo ${Vmess_UUID}
 
